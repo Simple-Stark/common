19ce4207ba164a40fcb2c4a13125cecf16a6d8f4
Simple <wrh_1125@163.com>
2021-09-22 22:27:43 +0800

    feat(全局统一消息返回): 全局统一消息返回

---
+++ src/main/java/com/simple/common/result/CodeMsg.java
@@ -0,0 +1,50 @@
+package com.simple.common.result;
+
+import lombok.Data;
+
+/**
+ * 全局统一消息与返回消息对应类
+ *
+ * @author Simple
+ * @date 2021/9/22 22:08
+ */
+@Data
+public class CodeMsg {
+    /**
+     * 全局通用错误码
+     */
+    public static CodeMsg BASE_SUCCESS = new CodeMsg(200, "success");
+    public static CodeMsg BASE_ERROR = new CodeMsg(100, "服务端异常");
+    public static CodeMsg BASE_BIND_ERROR = new CodeMsg(400, "参数校验异常：%s");
+    public static CodeMsg BASE_BUSINESS_ERROR = new CodeMsg(500, "业务异常通知：%s");
+
+    /**
+     * 响应状态码
+     */
+    private Integer code;
+    /**
+     * 响应信息
+     */
+    private String msg;
+
+    /**
+     * 【构造器】私有构造器，防止直接创建
+     * @param code 自定义响应码
+     * @param msg 自定义消息通知
+     */
+    private CodeMsg(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    /**
+     * 【工具方法】 补充指定错误码信息
+     * @param args 补充信息
+     * @return CodeMsg 实例
+     */
+    public CodeMsg fillArgs(Object... args) {
+        int code = this.code;
+        String message = String.format(this.msg, args);
+        return new CodeMsg(code, message);
+    }
+}
--- src/main/java/com/simple/common/result/Result.java
+++ src/main/java/com/simple/common/result/Result.java
@@ -6,7 +6,63 @@ package com.simple.common.result;
  * @author Simple
  * @date 2021/9/21 11:30
  */
-public class Result {
+public class Result<T> {
 
+    /**
+     * 全局消息统一返回 响应码
+     */
+    private final Integer code;
+    /**
+     * 全局消息统一返回 响应消息
+     */
+    private final String msg;
+    /**
+     * 全局消息统一返回 响应数据
+     */
+    private final T data;
+
+    /**
+     * 构造器私有，防止直接创建
+     */
+    private Result(Integer code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    /**
+     * 【构造器】私有构造器
+     * @param codeMsg 全局统一消息返回
+     * @param data 返回数据
+     */
+    private Result(CodeMsg codeMsg, T data) {
+        this.code = codeMsg.getCode();
+        this.msg = codeMsg.getMsg();
+        this.data = data;
+    }
+
+    public static Result<Void> success() {
+        return new Result<>(CodeMsg.BASE_SUCCESS, null);
+    }
+
+    public static <T> Result<T> success(T data) {
+        return new Result<>(CodeMsg.BASE_SUCCESS, data);
+    }
+
+    public static Result<Void> error() {
+        return new Result<>(CodeMsg.BASE_ERROR, null);
+    }
+
+    public static <T> Result<T> error(T data) {
+        return new Result<>(CodeMsg.BASE_ERROR, data);
+    }
+
+    public static Result<Void> error(CodeMsg CodeMsg) {
+        return new Result<>(CodeMsg, null);
+    }
+
+    public static Result<Void> error(String msg) {
+        return new Result<>(CodeMsg.BASE_ERROR.getCode(),msg, null);
+    }
 
 }
