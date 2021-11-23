30d6fe9caebfd9ee0de175ef38f11ca788d95065
Simple <wrh_1125@163.com>
2021-11-23 14:27:56 +0800

    feat(全局): 集成Mybatis-Plus 分页插件

---
+++ src/main/java/com/simple/common/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package com.simple.common.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Mybatis-Plus 配置类
+ * @author Simple 2021/11/23
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 【Bean】Mybatis Plus 分页插件
+     * @return com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor
+     * @author Simple
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
