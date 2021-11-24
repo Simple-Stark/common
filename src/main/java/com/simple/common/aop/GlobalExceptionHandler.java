04f5ad14bb24a6cbb7ffcf4936d5f87361e34e95
Simple <wrh_1125@163.com>
2021-11-24 15:29:23 +0800

    feat(全局异常处理): 捕捉所有场景的参数校验异常

--- src/main/java/com/simple/common/aop/GlobalExceptionHandler.java
+++ src/main/java/com/simple/common/aop/GlobalExceptionHandler.java
@@ -7,10 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 
 /**
@@ -26,7 +28,7 @@ public class GlobalExceptionHandler {
     /**
      * 通用异常处理方法
      * @param e 未知异常
-     * @return com.wrh.basis.common.Result 实例
+     * @return 异常信息
      */
     @ExceptionHandler(Exception.class)
     @ResponseBody
@@ -36,18 +38,49 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * 前端传入参数校验处理方法
+     * 前端传参数校验BindException 处理
+     * 对象以表单形式传输时触发
      * @param e 参数校验异常
-     * @return com.wrh.basis.common.Result 实例
+     * @return 校验信息提示
      */
     @ExceptionHandler(BindException.class)
     @ResponseBody
     public Result<Void> error(BindException e) {
         // 打印日志
-        log.error("前端传入参数校验异常，BindExceptionHandler =====>> {}",e.getMessage(),e);
+        log.error("前端传参校验异常，BindExceptionHandler =====>> {}",e.getMessage(),e);
         List<ObjectError> errors = e.getAllErrors();
         ObjectError error = errors.get(0);
         String msg = error.getDefaultMessage();
+        return Result.error(CodeMsg.BASE_BIND_ERROR.fillArgs(msg));
+    }
+
+    /**
+     * 前端传参数校验ConstraintViolationException 处理
+     * 非实体对象传输，绑定单个
+     * @param e 参数校验异常
+     * @return 校验信息提示
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseBody
+    public Result<Void> error(ConstraintViolationException e) {
+        // 打印日志
+        log.error("前端传参校验异常，ConstraintViolationException =====>> {}",e.getMessage(),e);
+        String msg = e.getMessage().split(":")[1];
+        return Result.error(CodeMsg.BASE_BIND_ERROR.fillArgs(msg));
+    }
+
+    /**
+     * 前端传参数校验MethodArgumentNotValidException 处理
+     * 对象以Json形式传输
+     * @param e 参数校验异常
+     * @return 校验信息提示
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Result<Void> error(MethodArgumentNotValidException e) {
+        // 打印日志
+        log.error("前端传参校验异常，MethodArgumentNotValidException =====>> {}",e.getMessage(),e);
+        String msg = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
         return Result.error(CodeMsg.BASE_BIND_ERROR.fillArgs(msg));
     }
 
